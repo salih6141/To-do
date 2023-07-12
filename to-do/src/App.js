90fc019e4af020ce4f289d88c34194624635fd7d
komutS <komutsalih61@icloud.com>
2023-07-12 19:34:42 +0200

    project completed

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -4,8 +4,43 @@ import React from 'react';
 import { Button, Card, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+function Todo({todo, index, doneTodo, deleteTodo}) {
+  return(
+    <div className='todo'>
+      <span style={{ textDecoration: todo.isDone ? "line-through": ""}}>{todo.text}</span>
+      <div>
+        <Button variant="outline-succes" onClick={() => doneTodo(index)}>√</Button>{''}
+        <Button variant="outline-danger" onClick={() => deleteTodo(index)}>x</Button>
+      </div>
+    </div>
+  )
+}
+
+function FormTodo({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Label><b>Add Todo</b></Form.Label>
+        <Form.Control type='text' className='input' value={value} onChange={e => setValue(e.target.value)} placeholder='Add new item' />
+      </Form.Group>
+      <Button variant='primary mb-3' type='submit'>
+        Submit
+      </Button>
+    </Form>
+  )
+}
+
 function App() {
-  const [todos, setTodos] = react.useState([ //defining a javascript todo list wich will contain the todos
+  const [todos, setTodos] = React.useState([ //defining a javascript todo list wich will contain the todos
     {
       text: "test ToDo",
       isDone: false
@@ -37,7 +72,13 @@ function App() {
         <h1 className='text-center mb-4'>Todo</h1>
         <FormTodo addTodo={addTodo} />
         <div>
-          
+          {todos.map((todo, index) => (
+            <Card>
+              <Card.Body>
+                <Todo key={index} index={index} todo={todo} doneTodo={doneTodo} deleteTodo={deleteTodo} />
+              </Card.Body>
+            </Card>
+          ))}
         </div>
       </div>
     </div>
